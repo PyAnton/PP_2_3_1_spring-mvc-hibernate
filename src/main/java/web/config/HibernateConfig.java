070c7d149e2e140104d26609a7a56fa345c85552
Antonmak112 <pyanton112@gmail.com>
2023-08-25 21:27:37 +0500

    added home controller and fixed autowired and equals hash code for user

--- src/main/java/web/config/HibernateConfig.java
+++ src/main/java/web/config/HibernateConfig.java
@@ -21,8 +21,12 @@ import java.util.Properties;
 @EnableTransactionManagement
 @ComponentScan(value = "web")
 public class HibernateConfig {
+    private final Environment env;
+
     @Autowired
-    private Environment env;
+    public HibernateConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public DataSource getDataSource() {
